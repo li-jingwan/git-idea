859a525f8c7336a7e62b417be43df5058e65a764
li-jingwan <jingwanli@yeah.net>
2020-02-24 14:50:59 +0800

    新增practice工程

---
+++ practice/src/com/cmcc/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.cmcc.git;
+
+/**
+ * Created by lijingwan on 2020/2/24.
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
