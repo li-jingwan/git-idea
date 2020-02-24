b4342eca86a8f895a2d956abee484fab3aa47cea
li-jingwan <jingwanli@yeah.net>
2020-02-24 21:56:31 +0800

    TestDemo

---
+++ practice/src/com/cmcc/git/SecondClass.java
@@ -0,0 +1,11 @@
+package com.cmcc.git;
+
+/**
+ * Created by lijingwan on 2020/2/24.
+ */
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("SecondClass");
+    }
+}
---
+++ practice/src/com/cmcc/test/TestDemo.java
@@ -0,0 +1,10 @@
+package com.cmcc.test;
+
+/**
+ * Created by lijingwan on 2020/2/24.
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("testDemo");
+    }
+}
